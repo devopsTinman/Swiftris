7b94b4e053de525c10f8d4375fbbbfbe6c6e3aef
DevOps Tinman <devops.tinman@gmail.com>
2019-01-16 06:49:08 -0700

    Added Assets and Cleaned up Default Code

---
+++ Swiftris/Swiftris/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Swiftris
+//
+//  Created by Allbee, Eamon on 1/16/19.
+//  Copyright © 2019 Allbee, Eamon. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ Swiftris/Swiftris/GameViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GameViewController.swift
+//  Swiftris
+//
+//  Created by Allbee, Eamon on 1/16/19.
+//  Copyright © 2019 Allbee, Eamon. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
